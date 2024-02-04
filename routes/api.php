64c04ee4bd83fd8a1d92f2a8bf193f3a70b58f2d
NouraMohamed123 <nouramohamed091@gmail.com>
2024-02-04 22:57:30 +0200

    fix

--- routes/api.php
+++ routes/api.php
@@ -7,11 +7,11 @@ use App\Http\Controllers\Admin\CityController;
 use App\Http\Controllers\AppUser\ReviewController;
 use App\Http\Controllers\AppUser\ApartmentController;
 use App\Http\Controllers\AppUser\BookedApartmentController;
-<<<<<<< HEAD
+
 use App\Http\Controllers\AppUser\FavoriteController;
-=======
+
 use App\Http\Controllers\AppUser\UserProfileController;
->>>>>>> ef788dc2e94ec66b9fce3c392c93fbe7f98c2017
+
 
 /*
 |--------------------------------------------------------------------------
@@ -29,22 +29,16 @@ use App\Http\Controllers\AppUser\UserProfileController;
 Route::post('/review', [ReviewController::class, 'store']);
 Route::post('/review/{review}', [ReviewController::class, 'update']);
 Route::delete('/review/{review}', [ReviewController::class, 'destroy']);
-
 //////booked
 Route::get('/booked', [BookedApartmentController::class, 'index']);
 Route::get('/booked/{booked}', [BookedApartmentController::class, 'show']);
 Route::post('/booked', [BookedApartmentController::class, 'store']);
 Route::post('/booked/{Booked_apartment}', [BookedApartmentController::class, 'update']);
 Route::delete('/booked/{Booked_apartment}', [BookedApartmentController::class, 'destroy']);
-
-<<<<<<< HEAD
 //favorit route
-
 Route::post('/favorit', [FavoriteController::class, 'store']);
 Route::post('/favorit/{favorit}', [FavoriteController::class, 'update']);
 Route::delete('/favorit/{favorit}', [FavoriteController::class, 'destroy']);
-
-=======
 //apartments
 Route::post('/search/apartments', [ApartmentController::class, 'search']);
 ///user profile
@@ -52,5 +46,5 @@ Route::get('/user-review', [UserProfileController::class, 'reviews']);
 Route::get('/user-favorit', [UserProfileController::class, 'favorit']);
 Route::get('/user-booked', [UserProfileController::class, 'booked']);
 Route::get('/user-settings', [UserProfileController::class, 'settings']);
->>>>>>> ef788dc2e94ec66b9fce3c392c93fbe7f98c2017
+
 require __DIR__ . '/dashboard.php';
