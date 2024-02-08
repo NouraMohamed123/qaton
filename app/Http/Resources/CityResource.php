6b78cfeba5d74a6c1d44a361f54241354b1be5d1
NouraMohamed123 <nouramohamed091@gmail.com>
2024-02-08 19:10:47 +0200

    ff

--- app/Http/Resources/CityResource.php
+++ app/Http/Resources/CityResource.php
@@ -15,7 +15,7 @@ class CityResource extends JsonResource
     public function toArray($request)
     {
 
-        return array_merge(parent::toArray($request), ['image' => asset('uploads/city/' . $this->image) ]);
+        return parent::toArray($request);
 
     }
 }
