9489390552a06de5a0f63748cd5eaa9c35777779
NouraMohamed123 <nouramohamed091@gmail.com>
2024-03-13 02:50:27 +0200

    fc

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\CreateAdminUserSeeder;
+use Database\Seeders\PermissionTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(PermissionTableSeeder::class);
         $this->call(CreateAdminUserSeeder::class);
 
 
