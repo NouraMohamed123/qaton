bbf3f80795082f7e896ac9992969bd9629cdbde4
AhmedHany852 <ahanyebrahim@gmail.com>
2024-03-27 23:00:40 +0200

    ok

--- app/Http/Controllers/Admin/ReviewController.php
+++ app/Http/Controllers/Admin/ReviewController.php
@@ -23,16 +23,21 @@ class ReviewController extends Controller
         ], Response::HTTP_OK);
     }
 
-    public function show($id)
+    public function show($apartment_id)
     {
-        $review = Review::findOrFail($id);
-
+        $review = Review::where('apartment_id', $apartment_id)->first();
+    
+        if (!$review) {
+            return response()->json(['message' => 'Review not found for the given Apartment ID'], Response::HTTP_NOT_FOUND);
+        }
+    
         return response()->json([
             'status' => 'success',
             'message' => 'Review retrieved successfully',
             'data' => new ReviewsResource($review),
         ], Response::HTTP_OK);
     }
+    
     public function destroy($id)
     {
         $review = Review::find($id);
