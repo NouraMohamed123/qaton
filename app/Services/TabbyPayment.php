f51131d80b762f09d987134ff3b3e933e1c79c27
NouraMohamed123 <nouramohamed091@gmail.com>
2024-06-25 11:43:03 +0300

    add

--- app/Services/TabbyPayment.php
+++ app/Services/TabbyPayment.php
@@ -216,18 +216,22 @@ class TabbyPayment
         $time = '';
         if ($type == 'booking') {
             $message = ControlNotification::where('type', 'booking')->value('message');
+            $title = ControlNotification::where('type', 'booking')->value('$title');
             $time = ControlNotification::where('type', 'booking')->value('time');
         } elseif ($type == 'entry_day') {
             $message = ControlNotification::where('type', 'entry_day')->value('message');
+            $title = ControlNotification::where('type', 'entry_day')->value('title');
             $time = ControlNotification::where('type', 'entry_day')->value('time');
         } elseif ($type == 'exit_day') {
             $message = ControlNotification::where('type', 'exit_day')->value('message');
+            $title = ControlNotification::where('type', 'exit_day')->value('title');
             $time = ControlNotification::where('type', 'exit_day')->value('time');
         } else {
             $message = 'Default message';
-            $time = '';
+            $title = 'Default title';
+            $time = 'Default time';
         }
 
-        return ['message' => $message, 'time' => $time];
+        return ['message' => $message, 'time' => $time , 'title' => $title];
     }
 }
